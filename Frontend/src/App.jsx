843c5922e0b7d6758e4ce1fd42d460f9794b21d7
ManishPandey21 <Pandey.manishkumar@gmail.com>
2025-02-11 01:57:06 +0530

    Create App.jsx

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/upload"
+          element={
+            <PrivateRoute>
+              <Upload />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
